54d22b35ed880cfb941eca9736c2081cdde733c4
CsgeeksYoutube <vijaymanral44@gmail.com>
2017-08-13 13:49:17 +0530

    react-redux code
    react-redux code

---
+++ src/containers/book-detail.js
@@ -0,0 +1,30 @@
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+
+ class BookDetail extends Component{
+
+ 	render(){
+ 		if(!this.props.book){
+ 			return <div>select a book to get started.</div>;
+ 		}
+	
+		return(
+			<div>
+			<h1>Detail for:</h1>
+			<div>Title :{this.props.book.title}</div>
+			<div>Pages :{this.props.book.pages}</div>
+
+			</div>
+
+			);
+	}
+}
+
+function mapStateToProps(state){
+	return{
+		book:state.activeBook
+	};
+
+}
+
+export default connect(mapStateToProps)(BookDetail);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function(){
+	return [
+	{title : 'javascript', pages:101},
+	{title : 'harry potter', pages:1031},
+	{title : 'the dart tower', pages:1201},
+	{title : 'Got', pages:1010},
+	{title : 'eloquent ruby', pages:1401}
+
+
+	]
+}
